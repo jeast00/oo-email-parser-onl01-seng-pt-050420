f01b9832122870ef110c55079a62a82a3161108d
Jonathan East <jonathan_east@ymail.com>
2020-05-23 20:49:53 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -17,7 +17,7 @@ class EmailAddressParser
       @email.split(", ")
     elsif @email.match(" ")
       @email.split(" ")
-    elsif @email.match(",") && @email.match(" ")
+    elsif @email.match(/\s|,/)
       @email.split(/\s|,/)
     end
   end
